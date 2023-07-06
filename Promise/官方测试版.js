36c13a577b58d6fe5e6e3803a0c2ccff7fb8418d
SiNanQrn <1987147904@qq.com>
2023-07-06 22:34:30 +0800

    测试版Promise

---
+++ Promise/官方测试版.js
@@ -0,0 +1,162 @@
+const PENDING = "pending";
+const FULFILLED = "fulfilled";
+const REJECTED = "reject";
+
+class Promise {
+  constructor(executor) {
+    if (typeof executor !== "function") {
+      throw new TypeError(`Promise resolver ${executor} is not a function`);
+    }
+
+    this.value = null; // 终值
+    this.reason = null; // 拒因
+    this.state = PENDING; // 状态
+    this.onFulfilledCallbacks = []; // 成功回调
+    this.onRejectedCallbacks = []; // 失败回调
+
+    const resolve = (value) => {
+      if (this.state === PENDING) {
+        this.state = FULFILLED;
+        this.value = value;
+        this.onFulfilledCallbacks.forEach((fn) => fn(value));
+      }
+    };
+
+    const reject = (reason) => {
+      if (this.state === PENDING) {
+        this.state = REJECTED;
+        this.reason = reason;
+        this.onRejectedCallbacks.forEach((fn) => fn(reason));
+      }
+    };
+
+    try {
+      executor(resolve, reject);
+    } catch (e) {
+      this.reject(e);
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    // 参数校检
+    onFulfilled =
+      typeof onFulfilled === "function" ? onFulfilled : (value) => value;
+    onRejected =
+      typeof onRejected === "function"
+        ? onRejected
+        : (r) => {
+            throw r;
+          };
+
+    // 实现链式调用, 且改变了后面then的值, 必须通过新的实例
+    let promise2 = new Promise((resolve, reject) => {
+      if (this.state === FULFILLED) {
+        setTimeout(() => {
+          try {
+            const x = onFulfilled(this.value);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (e) {
+            reject(e);
+          }
+        });
+      }
+
+      if (this.state === REJECTED) {
+        setTimeout(() => {
+          try {
+            const x = onRejected(this.reason);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (e) {
+            reject(e);
+          }
+        });
+      }
+
+      if (this.state === PENDING) {
+        this.onFulfilledCallbacks.push((value) => {
+          setTimeout(() => {
+            try {
+              const x = onFulfilled(value);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (e) {
+              reject(e);
+            }
+          });
+        });
+
+        this.onRejectedCallbacks.push((reason) => {
+          setTimeout(() => {
+            try {
+              const x = onRejected(reason);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (e) {
+              reject(e);
+            }
+          });
+        });
+      }
+    });
+    return promise2;
+  }
+}
+
+function resolvePromise(promise2, x, resolve, reject) {
+  // x 与 promise 相等
+  if (promise2 === x) {
+    reject(new TypeError("Chaining cycle detected for promise"));
+  }
+
+  let called = false;
+  if (x instanceof Promise) {
+    // 判断 x 为 Promise
+    x.then(
+      (value) => {
+        resolvePromise(promise2, value, resolve, reject);
+      },
+      (reason) => {
+        reject(reason);
+      }
+    );
+  } else if ((x !== null && typeof x === "object") || typeof x === "function") {
+    // x 为对象或函数
+    try {
+      const then = x.then;
+      if (typeof then === "function") {
+        then.call(
+          x,
+          (value) => {
+            if (called) return;
+            called = true;
+            resolvePromise(promise2, value, resolve, reject);
+          },
+          (reason) => {
+            if (called) return;
+            called = true;
+            reject(reason);
+          }
+        );
+      } else {
+        if (called) return;
+        called = true;
+        resolve(x);
+      }
+    } catch (e) {
+      if (called) return;
+      called = true;
+      reject(e);
+    }
+  } else {
+    resolve(x);
+  }
+}
+
+Promise.defer = Promise.deferred = function () {
+  let dfd = {};
+  dfd.promise = new Promise((resolve, reject) => {
+    dfd.resolve = resolve;
+    dfd.reject = reject;
+  });
+  return dfd;
+};
+module.exports = Promise;
+// promises-aplus-tests xxx
